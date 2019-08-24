166cd2eef6e4562cc36214839af4068de3280d1a
Łukasz Reszetow <lukasz.reszetow@birghtinventions.pl>
2019-08-24 11:30:53 +0200

    Zmiany

--- app/src/main/java/com/example/androidclient/MainActivity.kt
+++ app/src/main/java/com/example/androidclient/MainActivity.kt
@@ -1,113 +1,86 @@
 package com.example.androidclient
 
-import android.graphics.BitmapFactory
-import android.media.MediaPlayer
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.SystemClock
 import android.util.Base64
 import android.util.Log
 import android.widget.Toast
-import com.github.nkzawa.emitter.Emitter
-import com.github.nkzawa.socketio.client.IO
+import io.socket.client.IO
+import io.socket.client.Socket
+import io.socket.emitter.Emitter
 import kotlinx.android.synthetic.main.activity_main.*
-import java.io.File
-import java.io.FileInputStream
-import java.io.FileOutputStream
 import java.net.URISyntaxException
 
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var socket: com.github.nkzawa.socketio.client.Socket
-    var emitted: String = ""
-    var connectionTime: Long = 0
+    lateinit var socket: Socket
+    var wyemitowanaWiadomosc: String = ""
+    var czasKomunikacji: Long = 0
+    var startConnection: Long = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        try {
-            socket = IO.socket("http://10.0.2.2:1337/")
-            socket.connect()
-            socket.on("image", onNewImage)
-            socket.on("sound", onNewSound)
-            socket.on("pdf", onNewPdf)
-            Toast.makeText(this, "I think it worked", Toast.LENGTH_SHORT).show()
-        } catch (e: URISyntaxException) {
-            Toast.makeText(this, "Uri syntax exception", Toast.LENGTH_SHORT).show()
+
+        connectToServer.setOnClickListener {
+            connectToServer()
         }
 
         sendImage.setOnClickListener {
-            sendMessage(R.drawable.stars, "image")
+            sendMessage(R.drawable.gwiazdy, "obraz")
         }
 
         sendSound.setOnClickListener {
-            sendMessage(R.raw.small_sound, "sound")
+            sendMessage(R.raw.krotki_dzwiek, "dzwiek")
         }
 
         sendPdf.setOnClickListener {
-            sendMessage(R.raw.medium_pdf_file, "pdf")
+            sendMessage(R.raw.sredni_plik_pdf, "pdf")
         }
     }
 
-    private fun sendMessage(resourceToSend: Int, event: String) {
-        var message = ""
-        val thread = Thread(Runnable {
-            val start = SystemClock.elapsedRealtime()
-            val array = resources.openRawResource(+resourceToSend).readBytes()
-            message = Base64.encodeToString(array, Base64.DEFAULT)
-            emitted = message
-            Log.d("Tag", "Converting time was : ${SystemClock.elapsedRealtime() - start}")
-        })
-        thread.start()
-        thread.join()
-        connectionTime = SystemClock.elapsedRealtime()
-        socket.emit(event, message)
-    }
-
-    private val onNewImage: Emitter.Listener = Emitter.Listener {
-        runOnUiThread {
-            Log.d("Tag", "Connection time was : ${SystemClock.elapsedRealtime() - connectionTime}")
-            val value = it[0] as String
-            Log.d("Tag", "Image EMITED SAME = ${emitted == value}")
-//            val decoded: ByteArray = Base64.decode(value, Base64.DEFAULT)
-//            val bitmap = BitmapFactory.decodeByteArray(decoded, 0, decoded.size)
-//            imageView.setImageBitmap(bitmap)
+    private fun connectToServer() {
+        try {
+            val ip = IPaddress.text.toString()
+            socket = IO.socket("http://$ip:1337/")
+            socket.on(Socket.EVENT_CONNECT, polaczono)
+            socket.on("obraz", nowaWiadomoscZSerwera)
+            socket.on("dzwiek", nowaWiadomoscZSerwera)
+            socket.on("pdf", nowaWiadomoscZSerwera)
+            startConnection = SystemClock.elapsedRealtime()
+            socket.connect()
+        } catch (e: URISyntaxException) {
+            Toast.makeText(this, "Wyjątek zwiazany ze skladnia URI", Toast.LENGTH_SHORT).show()
         }
     }
 
-    private val onNewSound: Emitter.Listener = Emitter.Listener {
-        runOnUiThread {
-            Log.d("Tag", "Connection time was : ${SystemClock.elapsedRealtime() - connectionTime}")
-            val value = it[0] as String
-            Log.d("Tag", "Sound EMITED SAME = ${emitted == value}")
-//            val decoded: ByteArray = Base64.decode(value, Base64.DEFAULT)
-//            val path = File("$cacheDir/musicfile.3gp")
-//            val fos = FileOutputStream(path)
-//            fos.write(decoded)
-//            fos.close()
-//            val mediaPlayer = MediaPlayer()
-//            mediaPlayer.setDataSource("$cacheDir/musicfile.3gp")
-//
-//            mediaPlayer.prepare()
-//            mediaPlayer.start()
-        }
+    private fun sendMessage(plikDoWyslania: Int, naglowek: String) {
+        val start = SystemClock.elapsedRealtime()
+        val tablicaBitow = resources.openRawResource(+plikDoWyslania).readBytes()
+        val wiadomosc = Base64.encodeToString(tablicaBitow, Base64.DEFAULT)
+        wyemitowanaWiadomosc = wiadomosc
+        Log.d("AndroidClient", "Czas konwersji : ${SystemClock.elapsedRealtime() - start}")
+        czasKomunikacji = SystemClock.elapsedRealtime()
+        socket.emit(naglowek, wiadomosc)
     }
 
-    private val onNewPdf: Emitter.Listener = Emitter.Listener {
+    private val polaczono: Emitter.Listener = Emitter.Listener {
+        Log.d("AndroidClient", "Czas nawiazywania polaczenia: ${SystemClock.elapsedRealtime() - startConnection}")
+    }
+
+    private val nowaWiadomoscZSerwera: Emitter.Listener = Emitter.Listener {
         runOnUiThread {
-            Log.d("Tag", "Connection time was : ${SystemClock.elapsedRealtime() - connectionTime}")
-            val value = it[0] as String
-            Log.d("Tag", "pdf EMITED SAME = ${emitted == value}")
-//            val decoded: ByteArray = Base64.decode(value, Base64.DEFAULT)
-//            val bitmap = BitmapFactory.decodeByteArray(decoded, 0, decoded.size)
-//            imageView.setImageBitmap(bitmap)
+            Log.d("AndroidClient", "Komunikacja trwala : ${SystemClock.elapsedRealtime() - czasKomunikacji}")
+            val wiadomoscZSerwera = it[0] as String
+            Log.d("AndroidClient", "pliki sa identyczne? = ${wyemitowanaWiadomosc == wiadomoscZSerwera}")
         }
     }
 
     override fun onDestroy() {
         super.onDestroy()
         socket.disconnect()
-        socket.off("message", onNewImage)
+        socket.off("message", nowaWiadomoscZSerwera)
     }
 }
